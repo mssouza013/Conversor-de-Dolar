83d793bae970348056d492865b355abc06d4c19c
mssouza013 <aomatheussouza013@gmail.com>
2023-02-28 15:46:54 -0300

    Add project files.

---
+++ Csharpteste/ConversorDeMoeada.cs
@@ -0,0 +1,12 @@
+﻿namespace Csharpteste
+{
+    internal class ConversorDeMoeada
+    {
+        public static double Iof = 6.0; 
+        public static double Conversor (double dolar, double comprar)
+        {
+            double total = comprar * dolar;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ Csharpteste/Teste.cs
@@ -0,0 +1,18 @@
+﻿using Csharpteste;
+using System;
+using System.Globalization;
+
+namespace CsharpTeste;
+class Teste
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Qual a cotação do dólar? ");
+        double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantos dólares você vai comprar? ");
+        double comprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double result = ConversorDeMoeada.Conversor(dolar, comprar);
+        Console.Write("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
